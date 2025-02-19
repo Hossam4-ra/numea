921ece2524c9e0f390ca8395e0c1ab0aaca70a83
Hossam4-ra <housamrachd@gmail.com>
2025-02-19 18:01:16 +0100

    ttba

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
-import { BiBorderRadius } from 'react-icons/bi';
 import { Link } from 'react-router-dom';
 import ThemeToggle from './ThemeToggle';
 
 const Header = () => {
+  const style={backgroundColor:"black",color:"white",borderRadius: "5px",padding:"5px"}
   return (
     <Navbar bg="light" expand="lg">
       <Navbar.Brand as={Link} to="/">NUMEA</Navbar.Brand>
@@ -14,7 +14,7 @@ const Header = () => {
           <Nav.Link as={Link} to="/">Accueil</Nav.Link>
           <Nav.Link as={Link} to="/a-propos">À Propos</Nav.Link>
           <Nav.Link as={Link} to="/nos-services">Nos Services</Nav.Link>
-          <Nav.Link as={Link} to="/contact"><button>Nous rejoindre</button></Nav.Link>
+          <Nav.Link as={Link} to="/contact"><button style={style}>Nous rejoindre</button></Nav.Link>
         </Nav>
         <ThemeToggle />
       </Navbar.Collapse>
