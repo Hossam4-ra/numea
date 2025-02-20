a36717530afd1e394a5ef57784e8e4d83ce10cf7
mohamed <mohamedelanmbri@gmail.com>
2025-02-20 22:06:07 +0100

    february20

--- src/pages/AProps.jsx
+++ src/pages/AProps.jsx
@@ -4,6 +4,8 @@ import './AProps.css';
 import { FaShieldAlt, FaNetworkWired, FaBriefcase, FaChalkboardTeacher } from 'react-icons/fa';
 import image1 from '../assets/images/first.jpeg';
 import image2 from '../assets/images/second.jpg';
+import mission from '../assets/images/mission.jpg';
+import engagement from '../assets/images/engagement.jpg';
 const APropos = () => {
   return (
     <div>
@@ -27,19 +29,39 @@ const APropos = () => {
                 </div>
               </div>
               <div className="mission">
-                <h3>Notre Mission</h3>
-                <p>Notre stratégie s'appuie sur une vision globale de la protection de toute catégorie de l'information, ainsi que la digitalisation des processus de nos clients. En mettant à disposition de nos clients un Système d'Information et un Système Informatique fiables et sécurisés, qui leur permettent de réaliser leurs objectifs stratégiques avec la qualité visée. Ayant un savoir et une expertise dans le domaine, nous offrons à nos clients un package complet de conseil et de service et nous les accompagnons dans leurs projets SI.</p>
-                <div classNameName="about-image">
-                  <img src={image1} alt="about" />
+              <div className="mission-container">
+                <div className="mission-text">
+                  <h3>Notre Mission</h3>
+                  <p>
+                    Notre stratégie s'appuie sur une vision globale de la protection de toute catégorie de
+                    l'information, ainsi que la digitalisation des processus de nos clients. En mettant à
+                    disposition de nos clients un Système d'Information et un Système Informatique fiables et
+                    sécurisés, qui leur permettent de réaliser leurs objectifs stratégiques avec la qualité visée.
+                    Ayant un savoir et une expertise dans le domaine, nous offrons à nos clients un package complet
+                    de conseil et de service et nous les accompagnons dans leurs projets SI.
+                  </p>
+                </div>
+                <div className="mission-image">
+                  <img src={mission} style={{ height: "300px" }} alt="about" />
                 </div>
               </div>
-              <div className="engagement">
-                <h3>Notre Engagement</h3>
-                <p>Accompagner nos clients afin de renforcer leur Leadership, en accélérant la transformation digitale de leur processus, pour maintenir et améliorer leur avantage compétitif avec la conservation des données et la performance ciblée de leur SI.</p>
-                <div classNameName="about-image">
-                  <img src={image1} alt="about" />
-                </div>
-              </div>
+            </div>
+
+
+            <div className="engagement">
+  <div className="engagement-container">
+    <div className="engagement-image">
+      <img src={engagement} style={{ height: "300px" }} alt="about" />
+    </div>
+    <div className="engagement-text">
+      <h3>Notre Engagement</h3>
+      <p>
+        Accompagner nos clients afin de renforcer leur Leadership, en accélérant la transformation digitale de leur processus, 
+        pour maintenir et améliorer leur avantage compétitif avec la conservation des données et la performance ciblée de leur SI.
+      </p>
+    </div>
+  </div>
+</div>
             </div>
           </section>
 
